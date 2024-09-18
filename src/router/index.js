4d5407a38a2da84348b4cfb0044cf32062d7aa7b
何苏静 <2806016157@qq.com>
2024-09-18 10:07:44 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,7 @@ const Home = lazy(() => import(/*webpackChunkName:"home" */"../views/home/home")
 const Login = lazy(() => import(/*webpackChunkName:"login" */"../views/login/login"))
 const Sift = lazy(() => import(/*webpackChunkName:"login" */"../views/sift/sift"))
 const Shopcart = lazy(() => import(/*webpackChunkName:"login" */"../views/shopcart/shopcart"))
-
 const About = lazy(() => import(/*webpackChunkName:"login" */"../views/about/about"))
-
-
 
 function GenerateRoutes() {
     const routes = useRoutes([
@@ -32,11 +29,7 @@ function GenerateRoutes() {
             path: "/about",
             element: <About />
         },
-
-
-
     ])
-
 
     return routes
 }
